4353b4875a252beb6fb2350f8a907fe819e4590d
Srushthi Neje <SRUSHTHI.VIDHYASAGAR-NEJE@CAPGEMINI.COM>
2025-03-06 13:07:22 +0530

    WebAPI

---
+++ OnlineRailwayReservationSystem/OnlineRailwayReservationSystem/Models/TrainQoutum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlineRailwayReservationSystem.Models;
+
+public partial class TrainQoutum
+{
+    public string TrainId { get; set; } = null!;
+
+    public string QuotaId { get; set; } = null!;
+
+    public virtual Quotum Quota { get; set; } = null!;
+
+    public virtual Train Train { get; set; } = null!;
+}
